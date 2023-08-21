10c3e0154388f77425396097debf344ec056ff90
Quan Guo <gq@QuansMacBookM1.lan>
2023-08-21 19:22:48 +0800

    Support two-rails detection

--- DCC Autoreverser_V1.0.ino
+++ DCC Autoreverser_V1.0.ino
@@ -1,4 +1,4 @@
-  
+
 //Autoreverser 1.0 (2020-12-25)
 // Created by | InvertLogic (https://www.youtube.com/c/InvertLogic)
 // DCC Isolated Autoreversing Loop Circuit for Model railway
@@ -6,31 +6,45 @@
 //D8 pin connected to the base of the NPN transister. connect in series 10k ohm resistor.
 
 
-#define acs712 A0 // define the analog input pin
-#define Trigger_Pin 8 // pin connected to NPN transitor 
+#define acs712 A1      // define the analog input pin
+#define acs712_2 A2
+#define Trigger_Pin 7  // pin connected to NPN transitor
 
-float vpp = 0.0048828125; // measure voltage on your arduino and divide it with 1024 to get this value
+float vpp = 0.0048828125;   // measure voltage on your arduino and divide it with 1024 to get this value
 float sensitivity = 0.185;  //sensetivity of in mv
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(acs712, INPUT);
+  pinMode(acs712_2, INPUT);
   pinMode(Trigger_Pin, OUTPUT);
   Serial.begin(115200);
+  Serial.println("OK!! ");  // wait to avoid false triggers
 }
 
 void loop() {
-int counts = analogRead(acs712)+5; // read analog signal from current sensor
-float voltage = counts * vpp; 
-voltage -= 2.5; // remove offset
+  int counts = analogRead(acs712) + 5;  // read analog signal from current sensor
+  float voltage = counts * vpp;
 
-float ampere = voltage / sensitivity; // divide by sensor sensetivity 
-  
-  Serial.println("Amps: " + String (ampere)); 
+  int counts_2 = analogRead(acs712_2) + 5;  // read analog signal from current sensor
+  float voltage2 = counts_2 * vpp;
 
-if (ampere > 1.0){ 
+  voltage -= 2.5;  // remove offset
+  voltage2 -= 2.5;
 
-digitalWrite(Trigger_Pin, !digitalRead(Trigger_Pin)); // toggle output
-delay(500); // wait to avoid false triggers
+  float ampere = voltage / sensitivity;  // divide by sensor sensetivity
+  float ampere2 = voltage2 / sensitivity; 
+ // Serial.println("Amps: " + String(ampere));
+
+
+  if ((ampere > 2.8 ) || (ampere2 > 2.8)) {
+    //Serial.println("Amps: " + String(ampere));
+
+    digitalWrite(Trigger_Pin, !digitalRead(Trigger_Pin));  // toggle output
+    //Serial.println("Reverse!! ");   
+    delay(300);                       // wait to avoid false triggers
+    
   }
- }
+}
+ 
+
